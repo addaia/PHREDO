51da07a3b95347becffae240ac376400fed56c51
addaia <98821707+addaia@users.noreply.github.com>
2025-02-27 11:53:04 +0000

    track chabges

--- TEST/TEST.ino
+++ TEST/TEST.ino
@@ -4,40 +4,38 @@
 #include "Motors.h"
 #include "Encoders.h"
 #include "Kinematics.h"
-#include "PID.h"
 #include <math.h>
 
 Motors_c motors;
 LineSensors_c line_sensors;
 Magnetometer_c magnetometer;
 Kinematics_c pose;
-PID_c left_pid;
 
-
+# define MAX_ENTRIES 220
+unsigned long times[MAX_ENTRIES];
+float distances[MAX_ENTRIES];
+int entryCount = 0;
 
 void testAdvance(float targetDistance, unsigned long updateInterval) {
+  unsigned long startTime = millis();
   unsigned long lastUpdateTime = millis();
-  motors.setPWM(40, 40);
+  motors.setPWM(200,200);
 
 
   while (sqrt(pow(pose.x, 2) + pow(pose.y, 2)) < targetDistance) {
     if (millis() - lastUpdateTime >= updateInterval) {
-      pose.update();
+      pose.update(); 
       
+      if (entryCount < MAX_ENTRIES) {
+        times[entryCount] = millis() - startTime;
+        distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
+        entryCount++;
+      }
       
-      // Update the PID controller with demand and current measurement of total counts
-      float l_pwm = left_pid.update( pose.last_e0, pose.last_e1 );
-    
-      // Adjust speed of left motor
-      motors.setPWM( l_pwm + 40, 40 );
-      
-    
       // Update the time for the next pose update.
       lastUpdateTime = millis();
     }
-      
   }
-
 
 
   motors.setPWM(0, 0);
@@ -48,12 +46,18 @@ void setup() {
   Serial.begin(9600);
   // Initialise components as before.
   motors.initialise();
-  left_pid.initialise( -1, -0.01, 0.0 ); // Negative because counts are negative
   setupEncoder0();
   setupEncoder1();
   pose.initialise(0, 0, 0);
-  testAdvance(200.0, 0.5);
+  testAdvance(800.0, 40);
 }
 
 void loop() {
+  for (int i = 0; i < entryCount; i++) {
+    Serial.print("Time: ");
+    Serial.print(times[i]);
+    Serial.print(" ms, Distance: ");
+    Serial.println(distances[i]);
+  }
+  delay(5000);
 }
