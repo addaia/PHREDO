1419ab0c17a33fd312c0c005cc8ccad3574dd36f
addaia <98821707+addaia@users.noreply.github.com>
2025-03-04 17:22:08 +0000

    update test

--- TEST/TEST.ino
+++ TEST/TEST.ino
@@ -4,29 +4,42 @@
 #include "Motors.h"
 #include "Encoders.h"
 #include "Kinematics.h"
+#include "PID.h"
+# include <Wire.h>
+# include <LSM6.h>
 #include <math.h>
+#define BUZZER_PIN 6
 
 Motors_c motors;
 LineSensors_c line_sensors;
 Magnetometer_c magnetometer;
 Kinematics_c pose;
+PID_c left_pid;
+LSM6 imu;
+
 
 # define MAX_ENTRIES 220
-unsigned long times[MAX_ENTRIES];
+float times[MAX_ENTRIES];
 float distances[MAX_ENTRIES];
 int entryCount = 0;
 
 void testAdvance(float targetDistance, unsigned long updateInterval) {
-  unsigned long startTime = millis();
   unsigned long lastUpdateTime = millis();
+  unsigned long startTime = millis();
   motors.setPWM(200,200);
 
 
   while (sqrt(pow(pose.x, 2) + pow(pose.y, 2)) < targetDistance) {
     if (millis() - lastUpdateTime >= updateInterval) {
       pose.update(); 
+
+      float l_pwm = left_pid.update( pose.last_e0, pose.last_e1 );
+      motors.setPWM(-l_pwm + 200, 200);
+      
       
       if (entryCount < MAX_ENTRIES) {
+        imu.read();
+
         times[entryCount] = millis() - startTime;
         distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
         entryCount++;
@@ -36,28 +49,63 @@ void testAdvance(float targetDistance, unsigned long updateInterval) {
       lastUpdateTime = millis();
     }
   }
-
-
   motors.setPWM(0, 0);
+  for (int i = 0; i < 5; i++) {
+    delay(updateInterval);
+    
+    if (entryCount < MAX_ENTRIES) {
+        pose.update(); 
+
+        times[entryCount] = millis() - startTime;
+        distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
+        entryCount++;
+      }
+  }
+
+
+  
+  //analogWrite( BUZZER_PIN, 120 );
   Serial.println("Test complete. Target distance reached.");
 }
 
 void setup() {
+
+  // IMU
+  Wire.begin();
+  if (!imu.init() ) {  // no..? :(
+
+    // Since we failed to communicate with the
+    // IMU, we put the robot into an infinite
+    // while loop and report the error.
+    while (1) {
+      Serial.println("Failed to detect and initialize IMU!");
+      delay(1000);
+    }
+  }
+  imu.enableDefault();
+
+  // plotter
   Serial.begin(9600);
-  // Initialise components as before.
+  
+  // initialise standard comps
   motors.initialise();
   setupEncoder0();
   setupEncoder1();
+  left_pid.initialise( -1, -0.01, 0.0 );
   pose.initialise(0, 0, 0);
   testAdvance(800.0, 40);
 }
 
 void loop() {
+
+
+
+  
   for (int i = 0; i < entryCount; i++) {
-    Serial.print("Time: ");
     Serial.print(times[i]);
-    Serial.print(" ms, Distance: ");
+    Serial.print(" ");
     Serial.println(distances[i]);
   }
   delay(5000);
+  
 }
