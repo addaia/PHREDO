0e2b437ea7cebffbd2323e8aa645d78f160a66ac
addaia <98821707+addaia@users.noreply.github.com>
2025-03-10 23:22:55 +0000

    updated IMU

--- TEST/TEST.ino
+++ TEST/TEST.ino
@@ -1,12 +1,11 @@
-// imports (provided in folder)
 #include "LineSensors.h"
 #include "Magnetometer.h"
 #include "Motors.h"
 #include "Encoders.h"
 #include "Kinematics.h"
 #include "PID.h"
-# include <Wire.h>
-# include <LSM6.h>
+#include <Wire.h>
+#include <LSM6.h>
 #include <math.h>
 #define BUZZER_PIN 6
 
@@ -17,95 +16,143 @@ Kinematics_c pose;
 PID_c left_pid;
 LSM6 imu;
 
-
-# define MAX_ENTRIES 220
-float times[MAX_ENTRIES];
-float distances[MAX_ENTRIES];
+#define MAX_ENTRIES 200
+float speeds[MAX_ENTRIES];
+unsigned long times[MAX_ENTRIES];
+//float distances[MAX_ENTRIES];
 int entryCount = 0;
 
+// acceleartor bias
+float ax_bias = 0.0;
+
+// normalisation factor, needs to be checked based on units
+const float NORMALIZATION_FACTOR = 1000.0;
+
+// parameters for a simple filter
+const int filterSize = 10;
+float filterBuffer[filterSize] = {0};
+int filterIndex = 0;
+float filterSum = 0.0;
+
+// moving average filter
+float filterAcceleration(float new_sample) {
+  filterSum -= filterBuffer[filterIndex];   // remove oldest sample
+  filterBuffer[filterIndex] = new_sample;       // insert new sample
+  filterSum += new_sample;                      // update sum
+  filterIndex = (filterIndex + 1) % filterSize; // wrap index
+  return filterSum / filterSize;                // return average
+  }
+
+// function to calibrate
+
+float calibrateAccelerometer() {
+  float bias = 0;
+  const int calibrationSamples = 1000;
+  
+  for (int i = 0; i < calibrationSamples; i++) {
+    imu.read();
+    bias += imu.a.x;
+    delay(50);  // Adjust delay if needed for a stable reading
+  }
+  bias /= calibrationSamples;
+  return bias;
+}
+
+
+
+
 void testAdvance(float targetDistance, unsigned long updateInterval) {
-  unsigned long lastUpdateTime = millis();
   unsigned long startTime = millis();
-  motors.setPWM(200,200);
-
-
+  unsigned long lastUpdateTime = millis();
+  motors.setPWM(200, 200);
+  
   while (sqrt(pow(pose.x, 2) + pow(pose.y, 2)) < targetDistance) {
     if (millis() - lastUpdateTime >= updateInterval) {
-      pose.update(); 
-
-      float l_pwm = left_pid.update( pose.last_e0, pose.last_e1 );
+      pose.update();
+  
+      float l_pwm = left_pid.update(pose.last_e0, pose.last_e1);
       motors.setPWM(-l_pwm + 200, 200);
-      
-      
+  
       if (entryCount < MAX_ENTRIES) {
         imu.read();
-
+        // sub bias
+        float raw_ax = imu.a.x;
+        float corrected_ax = raw_ax - ax_bias;
+        // normalise
+        float normalized_ax = corrected_ax / NORMALIZATION_FACTOR;
+        // filter
+        float filtered_ax = filterAcceleration(normalized_ax);
+  
+        speeds[entryCount] = filtered_ax;
+        //distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
         times[entryCount] = millis() - startTime;
-        distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
         entryCount++;
       }
-      
-      // Update the time for the next pose update.
+  
       lastUpdateTime = millis();
     }
   }
   motors.setPWM(0, 0);
+  
+  // take a few extra readings after stopping
   for (int i = 0; i < 5; i++) {
     delay(updateInterval);
-    
     if (entryCount < MAX_ENTRIES) {
-        pose.update(); 
-
-        times[entryCount] = millis() - startTime;
-        distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
-        entryCount++;
-      }
-  }
-
-
+      imu.read();
+      pose.update();
   
-  //analogWrite( BUZZER_PIN, 120 );
+      float raw_ax = imu.a.x;
+      float corrected_ax = raw_ax - ax_bias;
+      float normalized_ax = corrected_ax / NORMALIZATION_FACTOR;
+      float filtered_ax = filterAcceleration(normalized_ax);
+  
+      speeds[entryCount] = filtered_ax;
+      //distances[entryCount] = sqrt(pow(pose.x, 2) + pow(pose.y, 2));
+      times[entryCount] = millis() - startTime;
+      entryCount++;
+    }
+  }
+  
   Serial.println("Test complete. Target distance reached.");
 }
 
 void setup() {
-
-  // IMU
+  
+  Serial.begin(9600);
+  
+  // initialise the IMU
   Wire.begin();
-  if (!imu.init() ) {  // no..? :(
-
-    // Since we failed to communicate with the
-    // IMU, we put the robot into an infinite
-    // while loop and report the error.
+  if (!imu.init()) {
     while (1) {
       Serial.println("Failed to detect and initialize IMU!");
       delay(1000);
     }
   }
   imu.enableDefault();
-
-  // plotter
-  Serial.begin(9600);
   
-  // initialise standard comps
+  // rest
   motors.initialise();
   setupEncoder0();
   setupEncoder1();
-  left_pid.initialise( -1, -0.01, 0.0 );
+  left_pid.initialise(-1, -0.01, 0.0);
   pose.initialise(0, 0, 0);
+  
+  // calibrate
+  ax_bias = calibrateAccelerometer();
+  Serial.print("Calibrated accelerometer bias (X-axis): ");
+  Serial.println(ax_bias);
+  
+  // start
   testAdvance(800.0, 40);
 }
 
 void loop() {
-
-
-
-  
+  // print stored results
   for (int i = 0; i < entryCount; i++) {
-    Serial.print(times[i]);
+    Serial.print(times[i] );
     Serial.print(" ");
-    Serial.println(distances[i]);
+    Serial.println(speeds[i]);
   }
-  delay(5000);
   
+  delay(5000);
 }
